cbda7a55fa2a5f57900d75ebf1546a7cbbd86c89
Edson Junior <edsonkjr@gmail.com>
2020-06-10 00:45:49 -0300

    Consulta com vários critérios.

--- src/main/java/br/com/edson/workshopmongodb/resources/util/URL.java
+++ src/main/java/br/com/edson/workshopmongodb/resources/util/URL.java
@@ -2,6 +2,10 @@ package br.com.edson.workshopmongodb.resources.util;
 
 import java.io.UnsupportedEncodingException;
 import java.net.URLDecoder;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.TimeZone;
 
 public class URL {
 
@@ -12,4 +16,15 @@ public class URL {
 			return "";
 		}
 	}
+
+	public static Date convertDate(String txtDate, Date defaultDate) {
+		SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd");
+		sdf.setTimeZone(TimeZone.getTimeZone("GMT"));
+		try {
+			return sdf.parse(txtDate);
+		} catch (ParseException e) {
+			return defaultDate;
+		}
+	}
+
 }
